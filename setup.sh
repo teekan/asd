311bc6324006fef76a485894e2e18331cc1677ab
teekan <82342440+teekan@users.noreply.github.com>
2021-04-16 13:34:34 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ sudo apt update
 sudo apt install -y python3-pip firefox
 sudo pip3 install selenium
 sudo mv geckodriver /usr/local/bin/geckodriver
+sudo chmod 400 a.pem
 sudo nohup python3 code.py &
