3e6dd4dff451bf72f59a0ac6e0d7f7f8d3309ee1
teekan <82342440+teekan@users.noreply.github.com>
2021-04-15 18:15:00 -0700

    Update track.sh

--- track.sh
+++ track.sh
@@ -1,1 +1,1 @@
-ssh -i "a.pem" ubuntu@ec2-52-207-88-254.compute-1.amazonaws.com "echo '$HOSTNAME - date' >> log"
+ssh -i "a.pem" ubuntu@ec2-52-207-88-254.compute-1.amazonaws.com "echo '$HOSTNAME - $(date)' >> log"
