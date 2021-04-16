416ccc0ade4e858bbedb0aed28735489aa086277
blah <teekan@protonmail.com>
2021-04-15 20:20:44 -0400

    first commit

---
+++ code.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+opts = Options()
+opts.headless = True
+while True:
+	browser = webdriver.Firefox(options=opts)
+	browser.get("http://www.slutbags.tk")
+	ads = browser.find_elements_by_tag_name("iframe")
+	ads[0].click()
+	browser.switch_to.window(browser.window_handles[0])
+	ads[1].click()
+	browser.switch_to.window(browser.window_handles[0])
+	body = browser.find_elements_by_tag_name("body")
+	body[0].click()
+	for x in browser.window_handles:
+		browser.switch_to.window(x)
+		browser.close()
