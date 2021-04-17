a9fa07307f3e8f86a51ba8d369b0a337510faa36
root <root@ip-172-31-33-245.us-east-2.compute.internal>
2021-04-17 00:32:10 +0000

    asdasdasa

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+import time
 import subprocess
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
@@ -7,17 +8,23 @@ def hit():
 	try:
 		browser = webdriver.Firefox(options=opts)
 		browser.get("http://www.slutbags.tk")
-		ads = browser.find_elements_by_tag_name("iframe")
-		print(ads[0])
-		ads[0].click()
-		browser.switch_to.window(browser.window_handles[0])
-		print(ads[1])
-		ads[1].click()
-		browser.switch_to.window(browser.window_handles[0])
 		body = browser.find_elements_by_class_name("text-center")
+		print(browser.title)
 		print(body[0])
 		body[0].click()
 		browser.switch_to.window(browser.window_handles[1])
+		print(browser.title)
+		ad = browser.find_elements_by_tag_name("iframe")
+		print(ad[0])
+		ad[0].click()
+		time.sleep(2)
+		browser.switch_to.window(browser.window_handles[1])
+		print(browser.title)
+		print(ad[1])
+		ad[1].click()
+		time.sleep(2)
+		browser.switch_to.window(browser.window_handles[1])
+		print(browser.title)
 		for x in browser.window_handles:
 			browser.switch_to.window(x)
 			browser.close()
